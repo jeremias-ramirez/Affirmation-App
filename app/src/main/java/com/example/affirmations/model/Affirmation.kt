3d1e0caa651f923b46ac55e6072122994a0c9255
Jeremias <jeremias@localhost.localdomain>
2021-03-20 20:47:02 -0300

    v1.0

--- app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -1,10 +1,14 @@
 package com.example.affirmations.model
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 
 
 /*
  * al agregar el prefijo data, indicas que la clase se de tipo datos
  * los que obliga a por lo menos definirle una propiedad
  */
-data class Affirmation(val stringResourceId: Int) {
+data class Affirmation(
+        @StringRes val stringResourceId : Int,
+        @DrawableRes val imageResourceId : Int) {
 }
